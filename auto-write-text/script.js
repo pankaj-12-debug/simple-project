22d9c358818addcb421655dc3bb2cb7468eb4064
pankaj-12-debug <kajopkkumar12@gmail.com>
2022-11-07 15:29:30 +0530

    write-text-auto

---
+++ auto-write-text/script.js
@@ -0,0 +1,14 @@
+const text="we are best in the world";
+let index=0;
+function writeContex(){
+    document.body.innerText=text.slice(0,index);
+    index++;
+    if(index>text.length-1)
+    {
+        index=0;
+    }
+}
+//setInterval(writeContex,100);
+setInterval(() => {
+    writeContex();
+},100);
